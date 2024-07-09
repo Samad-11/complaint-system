851900644dc29df1e8939d23e6f7fc125f098f26
abdusSamadJIMS <abdussamadjims@gmail.com>
2024-07-09 15:36:07 +0530

    prisma error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,8 +7,8 @@ const globalForPrisma = global as unknown as {
 export const prisma = globalForPrisma || new PrismaClient();
 
 
-if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     globalForPrisma.prisma = prisma
+// }
 
 export default prisma;
